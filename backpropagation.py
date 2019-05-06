01028da9fe6928c91ee12f53afb850b9d42a5055
Lucas Antunes de Almeida <lucas.lukinha67@gmail.com>
2019-05-06 13:53:15 -0300

    first step

--- backpropagation.py
+++ backpropagation.py
@@ -1,36 +1,57 @@
 import argparse
 from _includes.readfile import *
 
-class neuron:
+class Neuron:
     def __init__(self, inputs):
         self.w = np.random.rand(inputs) 
         self.y = np.float32(0.0)
     
-    def h(self, inputs):
-        pass
+    def h(self, neuronInput):
+        # print(self.w)
+        # print(len(neuronInput))
+        self.y = np.float32(0.0)
 
-class network:
+class Network:
     def __init__(self, inputs, first, second, third):
         self.neurons = [
-            [neuron(inputs) for i in range(first)],
-            [neuron(first) for i in range(second)],
-            [neuron(second) for i in range(third)]
+            [Neuron(inputs) for i in range(first)],
+            [Neuron(first) for i in range(second)],
+            [Neuron(second) for i in range(third)]
         ]
 
-class backpropagation:
+class Backpropagation:
     def __init__(self, first, second, third, matrix):
         self.dataset = matrix
         self.datasetT = [getColumn(matrix, i) for i in range(len(matrix[0]))]
         self.x = self.datasetT[:-third]
         self.y = self.datasetT[-third:]
-        self.maxTime = 10000
-        self.network = network(len(self.x), first, second, third)
+        self.maxTime = 1
+        self.network = Network(len(self.x), first, second, third)
+        self.delta = self.y[:]
 
     def error(self, time):
         return (self.maxTime > time)
 
     def firstStage(self):
-        pass
+        # initialization that guarantees the input value of the first layer
+        neuronInput = self.x[:]
+        # presentation of the inputs to the network
+        # and propagation of the outputs to the network
+        for network in self.network.neurons:
+            temp = []
+            # passes to the current input to the current neuron 
+            # and saves the output of the neuron
+            for neuron in network:
+                neuron.h(neuronInput)
+                temp.append(neuron.y)
+            # copy of the output values of each layer
+            neuronInput = temp[:]
+        # error computation (desired-obtained)
+        for i in range(len(self.network.neurons[-1])):
+            self.delta[i] = self.y[i] - self.network.neurons[-1][i].y
+            # print(self.delta[i])
+            
+
 
     def secondStage(self):
         pass
@@ -40,7 +61,7 @@ class backpropagation:
 
     def training(self):
         time = 0
-
+ 
         while(self.error(time)):
             time += 1
             self.firstStage()
@@ -58,5 +79,5 @@ def arguments():
 
 if __name__ == "__main__":
     args = arguments()
-    backprop = backpropagation(args.first, args.second, args.third, readfile(args.input))
+    backprop = Backpropagation(args.first, args.second, args.third, readfile(args.input))
     backprop.training()
